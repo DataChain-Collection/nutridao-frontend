cf974f1411bb1247019b582c8976930a5df32af9
kmilo <kmilo.denis.glez@yandex.com>
2025-06-19 22:53:08 -0400

    remove old logo use dark theme by default

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -4,37 +4,38 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Lines from "@/components/Lines";
 import ScrollToTop from "@/components/ScrollToTop";
-import { ThemeProvider } from "next-themes";
-import { Inter } from "next/font/google";
+import {ThemeProvider} from "next-themes";
+import {Inter} from "next/font/google";
 import "../globals.css";
 import Web3Provider from "../context/Web3Provider";
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({subsets: ["latin"]});
 
 import ToasterContext from "../context/ToastContext";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`dark:bg-black ${inter.className}`}>
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={`dark:bg-black ${inter.className}`}>
         <ThemeProvider
-          enableSystem={false}
-          attribute="class"
-          defaultTheme="light"
+            enableSystem={false}
+            attribute="class"
+            defaultTheme="dark"
         >
-          <Web3Provider>
-            <Lines />
-            <Header />
-            <ToasterContext />
-            {children}
-            <Footer />
-            <ScrollToTop />
-          </Web3Provider>
+            <Web3Provider>
+                <Lines/>
+                <Header/>
+                <ToasterContext/>
+                {children}
+                <Footer/>
+                <ScrollToTop/>
+            </Web3Provider>
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
