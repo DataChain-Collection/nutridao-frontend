9ee737d7f7c4be3c27171d8a5d4f4a4f1424318f
ginrod <gginrod20@gmail.com>
2025-06-23 16:06:23 -0400

    add br to force a newline

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -21,6 +21,7 @@ const Hero = () => {
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
                 A DataChain Collection Initiative
+                <br />
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
                   Ethical, User-Owned Health Data
                 </span>
