79ec073cb5bb47eb59d9e6f3a10c09dc3d3c05cd
ginrod <gginrod20@gmail.com>
2025-06-23 16:51:42 -0400

    create basic be real FAQs

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,18 +3,18 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "What is NutriDAO and how does it work?",
+    ans: "NutriDAO is a community-driven DataDAO focused on ethical, user-owned nutritional data. Users can contribute structured supplement data and metadata, stored using FHIR standards and IPFS. In return, contributors receive rewards and help power AI models in health research.",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Do I need technical knowledge to contribute?",
+    ans: "No. NutriDAO is designed to be accessible. Whether you're a health enthusiast, practitioner, or developer, you can contribute data through a simple interface, and all contributions are reviewed and curated by the community.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: " How is my data protected?",
+    ans: "Your data is handled with strict consent mechanisms and stored in decentralized infrastructure. You decide what to share, and with whom. All access is governed by transparent smart contracts and aligned with ethical data principles. Additionally, the Vana ecosystem provides a robust foundation for security and privacy, ensuring your data remains protected throughout its lifecycle.",
   },
 ];
 
