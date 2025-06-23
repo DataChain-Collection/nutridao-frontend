c86dcdbddb1d873ef7eb816431f1050d77a971a7
ginrod <gginrod20@gmail.com>
2025-06-23 15:07:20 -0400

    rename metadata text from template to nutriDAO

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,10 +14,10 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  title: "NutriDAO",
 
   // other metadata
-  description: "This is Home for Solid Pro"
+  description: "NutriDAO"
 };
 
 export default function Home() {
